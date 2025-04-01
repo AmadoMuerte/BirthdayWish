cc16641e1786bda91e09124e0498f18836e835ff
AmadoMuerte <kisik854@gmail.com>
2025-04-02 02:29:26 +1000

    update header and hero background styles

--- Web/src/components/nav/nav.tsx
+++ Web/src/components/nav/nav.tsx
@@ -2,24 +2,38 @@ import { Link } from "@tanstack/react-router";
 import routes from "../../routes/routes.json";
 import styles from './nav.module.css'
 
+const iconModules = import.meta.glob('../../assets/icons/*.svg', { eager: true });
+
 type Route = {
     name: string;
     path: string;
+    icon?: string
+}
+
+function renderRoutes() {
+    return routes.map((route: Route) => {
+        const iconPath = route.icon ? `../../assets/icons/${route.icon}` : null;
+        const iconSrc = iconPath ? (iconModules[iconPath] as { default: string })?.default : null;
+
+        return (
+            <li key={route.name}>
+                <div className={styles.navItem}>
+                    {iconSrc && <img src={iconSrc} alt={route.name} />}
+                    <Link to={route.path}>
+                        {route.name}
+                    </Link>
+                </div>
+            </li>
+        );
+    })
 }
 
 export default function Nav() {
-
     return (
         <nav className={styles.nav}>
             <ul>
-                {routes.map((route: Route) => (
-                    <li key={route.name}>
-                        <Link to={route.path}>
-                            {route.name}
-                        </Link>{' '}
-                    </li>
-                ))}
+                {renderRoutes()}
             </ul>
         </nav>
-    )
+    );
 }
