1f45eb4b96de3fef5bc65f5410ffec0acd3004b7
Mazd3 <haziullin2000@gmail.com>
2026-02-10 12:00:00 +0500

    feat(widgets): implement header and profile components

---
+++ front/src/widgets/header/ui/Header.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Group from '@/shared/ui/Group.vue';
+import { UserMenu } from '@/features/user/user-menu';
+import { Button } from '@/shared/ui/Button';
+import { IconPlus } from '@tabler/icons-vue';
+</script>
+
+<template>
+    <header class="header">
+        <Group class="header__content" justify="between" align="center">
+            <div class="header__title">Wish</div>
+            <div class="header__actions">
+                <Button size="md" radius="sm" variant="outline">
+                    
+                        <IconPlus size="20" />
+                  
+                    Add wish
+                </Button>
+                <UserMenu />
+            </div>
+        </Group>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    width: 100%;
+    background: var(--background);
+    margin-bottom: var(--space-8);
+
+    &__content {
+        max-width: var(--content);
+        padding: 0 var(--space-4);
+        margin: 0 auto;
+        height: 64px;
+    }
+
+    &__title {
+        font-size: 32px;
+        font-weight: bold;
+    }
+
+    &__actions {
+        display: flex;
+        align-items: center;
+        gap: var(--space-2);
+    }
+}
+</style>
---
+++ front/src/widgets/profile-header/ui/ProfileHeader.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { Heading, Text } from '@/shared/ui/Typography';
+
+const { user } = defineProps<{
+    user: User,
+}>()
+</script>
+
+<template>
+    <div class="profile-header">
+        <img class="profile-header__banner" :src="user.banner" :alt="user.name + ' banner'">
+        <div class="profile-header__content">
+            <div class="profile-header__avatar">
+                <img :src="user.avatar" :alt="user.name + ' avatar'">
+            </div>
+            <div class="profile-header__info">
+                <Heading size="xl">{{ user.name }}</Heading>
+                <Text size="sm" weight="medium" color="muted">@{{ user.username }}</Text>
+            </div>
+            <div class="profile-header__actions">
+                <slot name="actions" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.profile-header {
+    display: flex;
+    flex-direction: column;
+    border-radius: var(--radius-xl);
+    overflow: hidden;
+    padding: var(--space-2);
+    padding-bottom: var(--space-0);
+    background-color: var(--card);
+    box-shadow: var(--shadow-sm);
+    border: 1px solid var(--border);
+}
+
+.profile-header__banner {
+    width: 100%;
+    height: 240px;
+    object-fit: cover;
+    border-radius: var(--radius-md);
+}
+
+.profile-header__content {
+    position: relative;
+    display: flex;
+    gap: var(--space-6);
+    padding-left: var(--space-6);
+    padding-right: var(--space-4);
+}
+
+.profile-header__avatar {
+    position: relative;
+    width: 200px;
+    margin: var(--space-4) 0;
+
+    img {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        aspect-ratio: 1;
+        object-fit: cover;
+        border-radius: 50%;
+        border: 4px solid var(--card);
+        box-shadow: var(--shadow-md);
+    }
+}
+
+.profile-header__info {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: var(--space-4) 0;
+}
+
+.profile-header__actions {
+    display: flex;
+    align-items: center;
+    gap: var(--space-3);
+    padding: var(--space-6) 0;
+}
+</style>
